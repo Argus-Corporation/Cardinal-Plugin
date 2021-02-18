b0a61d29ea74ac7f0b6d7c00519c869ec120dfe5
Argus-corporation <malaska@mailo.com>
2021-02-18 20:59:53 +0100

    Cardinal-Plugin update

--- src/net/argus/plugin/InitializationPlugin.java
+++ src/net/argus/plugin/InitializationPlugin.java
@@ -2,6 +2,7 @@ package net.argus.plugin;
 
 import net.argus.plugin.annotation.PluginInfo;
 import net.argus.system.AnnotationManager;
+import net.argus.system.Launcher;
 import net.argus.system.UserSystem;
 import net.argus.util.debug.Debug;
 
@@ -9,7 +10,7 @@ public class InitializationPlugin {
 	
 	public static void register() {
 		String cp = UserSystem.getProperty("class");
-		if(cp != null && cp.contains(";")) {
+		if(cp != null && cp.contains(Launcher.getSeperator())) {
 			String[] mainClasses = cp.split(";");
 			
 			for(String mainClass : mainClasses) {
@@ -20,8 +21,9 @@ public class InitializationPlugin {
 					PluginRegister.addPlugin((Plugin) cl.newInstance(), info);	
 					
 					Debug.log("Plugin \"" + info.pluginId() + "\" version \"" + info.version() + "\" is loaded");
-				}catch(InstantiationException | IllegalAccessException | ClassNotFoundException | PluginException e) {
+				}catch(InstantiationException | IllegalAccessException | ClassNotFoundException | ClassCastException | PluginException e) {
 					if(e instanceof ClassNotFoundException) Debug.log(mainClass + " not found");
+					if(e instanceof ClassCastException) Debug.log(mainClass + " not extends \"" + Plugin.class + "\"");
 				}
 			}
 		}
